5925a2c1d455cc29912a1eb7971116e1402c040d
Jeffrey LeCompte <jeffreylec@gmail.com>
2015-05-29 15:35:23 -0700

    Added some comments.

--- Programming Assignment/src/teamGuru/Main.java
+++ Programming Assignment/src/teamGuru/Main.java
@@ -1,6 +1,11 @@
 package teamGuru;
 
-// welcome! :D
+/*
+ * Authors: Robbie Nichols, Mike Overby, Ian McPeek, Jeffrey LeCompte
+ * Team: Team Guru
+ * Assignment: Programming Assignment
+ * Class: TCSS 343 Spring 2015
+ */
 
 public class Main {
 
